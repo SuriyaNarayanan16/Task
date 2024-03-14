70b069bd9e8935fc6637650a0a032f7b68b2a475
suriya <suriyanarayan416@gmail.com>
2024-03-14 15:05:41 +0530

    final push

--- src/globalContext/GlobalContext.js
+++ src/globalContext/GlobalContext.js
@@ -9,14 +9,8 @@ export const GlobalContext = ({ children }) => {
   const [categories, setCategories] = useState(Category);
   const [cart, setCart] = useState([]);
 
-  useEffect(() => {
-    console.log(cart, "item");
-  }, [cart]);
-
   const IncrementCart = (id, item) => {
-    console.log(id, item);
     let currenIndex = cart.findIndex((x) => x.id === id);
-    console.log(cart[currenIndex]);
     if (currenIndex === -1) {
       cart.push(item);
     } else {
@@ -26,7 +20,6 @@ export const GlobalContext = ({ children }) => {
   };
 
   const DecrementCart = (id, item) => {
-    console.log(item, "item");
     let currenIndex = cart.findIndex((x) => x.id === id);
     item.count === 0 ? cart.splice(currenIndex, 1) : (cart[currenIndex] = item);
     setCart([...cart]);
